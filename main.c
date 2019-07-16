71f743b15e0d8cdbcd9e8eacaa75397892a373b0
muchgangster <jbarringer1999@gmail.com>
2019-07-15 21:47:36 -0700

    Now can read size of file and create variable size array of binary

--- main.c
+++ main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 
 int main() {
-  unsigned char buffer[20];
+  // unsigned char buffer[20];
   FILE *stream_ptr;
+  stream_ptr = fopen("megaman.png", "rb");
 
-  stream_ptr = fopen("file", "rb");
+  long int file_size;
+  fseek(stream_ptr, 0L, SEEK_END);
+  file_size = ftell(stream_ptr);
+  rewind(stream_ptr);
+
+  unsigned char buffer[file_size];
+
   fread(buffer, sizeof(buffer), 1, stream_ptr);
 
-  for (size_t i = 0; i < 20; i++) {
+  for (size_t i = 0; i < file_size; i++) {
     printf("%u ", buffer[i]);
   }
 
