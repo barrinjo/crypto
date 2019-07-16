6ecede79621ceafe3ea34f10a32877b9f001e578
muchgangster <jbarringer1999@gmail.com>
2019-07-16 08:02:41 -0700

    personal encryption keys and fixed file writing

--- main.c
+++ main.c
@@ -1,22 +1,47 @@
 #include <stdio.h>
+#include <string.h>
+#define PRIVATE_KEY 14;
 
-int main() {
-  // unsigned char buffer[20];
-  FILE *stream_ptr;
-  stream_ptr = fopen("megaman.png", "rb");
+void encrypt(char file_name[], int private_key) {
+  FILE *ifstream_ptr;
+  ifstream_ptr = fopen(file_name, "rb");
 
   long int file_size;
-  fseek(stream_ptr, 0L, SEEK_END);
-  file_size = ftell(stream_ptr);
-  rewind(stream_ptr);
+  fseek(ifstream_ptr, 0L, SEEK_END);
+  file_size = ftell(ifstream_ptr);
+  rewind(ifstream_ptr);
 
   unsigned char buffer[file_size];
 
-  fread(buffer, sizeof(buffer), 1, stream_ptr);
+  fread(buffer, sizeof(buffer), 1, ifstream_ptr);
 
+  fclose(ifstream_ptr);
+
+  FILE *ofstream_ptr;
+  ofstream_ptr = fopen("output", "wb");
   for (size_t i = 0; i < file_size; i++) {
-    printf("%u ", buffer[i]);
+    // modify each binary value by the private key value to "encrypt file"
+    buffer[i] += private_key;
+    // printf("%uc ", buffer[i]);
   }
+  fwrite(buffer, sizeof(buffer), 1, ofstream_ptr);
+}
+
+int create_key() {
+  int input;
+  printf("enter random number set: ");
+  scanf("%d", &input);
+  return input % 30;
+}
+
+int main(int argc, char *argv[]) {
+  if(argc < 2) {
+    printf("no file name given.\n");
+  } else {
+
+    encrypt(argv[1], create_key());
+  }
+    
 
   return 0;
 }
