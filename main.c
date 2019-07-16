b1c4876227e6cfe3c1d7c484f52f18576c2a9fd5
muchgangster <jbarringer1999@gmail.com>
2019-07-15 21:36:00 -0700

    basic input binary file and output ascii

--- main.c
+++ main.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 
-void joshsuk(void) {
-  printf("josh suky\n");
-}
-
 int main() {
-    printf("john dumb\n");
-    joshsuk();
-    return 0;
+  unsigned char buffer[20];
+  FILE *stream_ptr;
+
+  stream_ptr = fopen("file", "rb");
+  fread(buffer, sizeof(buffer), 1, stream_ptr);
+
+  for (size_t i = 0; i < 20; i++) {
+    printf("%u ", buffer[i]);
+  }
+
+  return 0;
 }
