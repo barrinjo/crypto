138599ae31216b2b8139d0ae5fb54981c8330652
JohnNWarila <jnw.csgo@gmail.com>
2019-07-19 11:46:58 -0700

    Added some stuff for rsa, still mostly broken

--- rsa.c
+++ rsa.c
@@ -1,26 +1,62 @@
 #include <stdio.h>
 #include <math.h>
+#include <string.h>
 
-void rsa() {
-    int prime1 = 53;
-    int prime2 = 59;
+#define MAX_LENGTH 256
 
-    int k = 2;
-    int e = 3;
-
-    long long n = prime1 * prime2;
-
-    long long thetan = (prime1-1)*(prime2-1);
-
-    long long private_key = (k * thetan + 1) / e;
-
-    char msg[] = "Test";
-    char encrypted[4];
-    char temp;
-
-    for (int i = 0; i < 4; ++i) {
-        temp = (msg[i]^e) % n;
-        encrypted[i] = temp;
-        printf("%d\n", temp);
+int gcd(int a, int h) {
+    int temp;
+    while(1) {
+        temp = a % h;
+        if (temp == 0) {
+            return h;
+        }
+        a = h;
+        h = temp;
     }
+}
+
+void rsaEncrypt(char msg[], double prime_1, double prime_2, int k) {
+
+    double n = prime_1 * prime_2;
+
+    double phi = (prime_1-1)*(prime_2-1);
+    int e = 2;
+
+    while (e < phi) {
+        if (gcd(e, phi) == 1)
+            break;
+        else
+            ++e;
+    }
+
+    long long private_key = ((k * phi) + 1) / e;
+
+    printf("%lld\n", private_key);
+
+    char temp;
+    char encrypted_msg[MAX_LENGTH];
+    char decrypted_msg[MAX_LENGTH];
+
+    for (int i = 0; i < strlen(msg); ++i) {
+        temp = fmod(pow(msg[i], e), n);
+        encrypted_msg[i] = temp;
+        printf("%c\n", encrypted_msg[i]);
+    }
+
+    for (int i = 0; i < strlen(msg); ++i) {
+        decrypted_msg[i] = fmod(pow(encrypted_msg[i], private_key), n);
+        printf("%c\n", decrypted_msg[i]);
+    }
+
+}
+
+int main (int argc, char *argv[]) {
+    char msg[] = "test";
+    double prime_1 = 13;
+    double prime_2 = 17;
+    int k = 2;
+
+    rsaEncrypt(msg, prime_1, prime_2, k);
+    return 0;
 }
